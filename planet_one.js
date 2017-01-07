e8d1fc2af192d58b59232b12fde402ad065a6eca
Petr Ševčík <sevcik.cartography@gmail.com>
2017-01-07 12:37:48 +0100

    s

--- planet_one.js
+++ planet_one.js
@@ -1,10 +1,12 @@
 var planet = planetaryjs.planet();
 // You can remove this statement if `world-110m.json`
 // is in the same path as the HTML page:
-//planet.loadPlugin(planetaryjs.plugins.earth({
-// topojson: { file: 'world-110m.json' }
-// }));
+planet.loadPlugin(planetaryjs.plugins.earth({
+	topojson: { file: 'world-110m.json' }
+	}));
 // Make the planet fit well in its canvas
+planet.loadPlugin(planetaryjs.plugins.drag());
+
 planet.projection.scale(250).translate([250, 250]);
 var canvas = document.getElementById('globe');
 planet.draw(canvas);
