3ac515cb1b31f277b1cc43aa40025ce0b708f020
Petr Ševčík <sevcik.cartography@gmail.com>
2017-01-26 20:06:03 +0100

    ss

--- planet_one.js
+++ planet_one.js
@@ -1,11 +1,53 @@
 var planet = planetaryjs.planet();
 // You can remove this statement if `world-110m.json`
 // is in the same path as the HTML page:
+planet.loadPlugin(autorotate(10));
 planet.loadPlugin(planetaryjs.plugins.earth({
   topojson: { file: 'world-110m.json' }
 }));
-planet.loadPlugin(planetaryjs.plugins.drag());
+
+planet.loadPlugin(planetaryjs.plugins.drag({
+    // Dragging the globe should pause the
+    // automatic rotation until we release the mouse.
+    onDragStart: function() {
+      this.plugins.autorotate.pause();
+    },
+  }));
+
+
 // Make the planet fit well in its canvas
-planet.projection.scale(250).translate([250, 250]);
+planet.projection.scale(350).translate([350, 350]).rotate([0, -10, 0]);
+
+
 var canvas = document.getElementById('globe');
 planet.draw(canvas);
+
+
+function autorotate(degPerSec) {
+    // Planetary.js plugins are functions that take a `planet` instance
+    // as an argument...
+    return function(planet) {
+      var lastTick = null;
+      var paused = false;
+      planet.plugins.autorotate = {
+        pause:  function() { paused = true;  },
+        resume: function() { paused = false; }
+      };
+      // ...and configure hooks into certain pieces of its lifecycle.
+      planet.onDraw(function() {
+        if (paused || !lastTick) {
+          lastTick = new Date();
+        } else {
+          var now = new Date();
+          var delta = now - lastTick;
+          // This plugin uses the built-in projection (provided by D3)
+          // to rotate the globe each time we draw it.
+          var rotation = planet.projection.rotate();
+          rotation[0] += degPerSec * delta / 1000;
+          if (rotation[0] >= 180) rotation[0] -= 360;
+          planet.projection.rotate(rotation);
+          lastTick = now;
+        }
+      });
+    };
+  };
