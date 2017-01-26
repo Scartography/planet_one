d434b05b26b69c5d3436a868441518473ec4146b
Petr Ševčík <sevcik.cartography@gmail.com>
2017-01-26 19:41:57 +0100

    s

--- planet_one.js
+++ planet_one.js
@@ -1,23 +1,12 @@
 var planet = planetaryjs.planet();
 
-planet.loadPlugin(planetaryjs.plugins.oceans({
-  fill: 'cyan'
-}));
 
-planet.loadPlugin(planetaryjs.plugins.drag());
-
-planet.loadPlugin(function(planet) {
-  planet.onDraw(function () {
-    planet.withSavedContext(function (context) {
-      var arc = {type: "LineString", coordinates: [[40, 30], [40, -50]]};
-      context.beginPath();
-      planet.path.context(context)(arc);
-      context.stroke();
-      context.closePath();
-    });
-  });
-});
-
-planet.projection.scale(250).translate([250, 250]);
-var canvas = document.getElementById('globe');
-planet.draw(canvas);
+planet.loadPlugin(planetaryjs.plugins.earth({
+    topojson: { file: '/world-110m.json' }
+  }));
+  // Scale the planet's radius to half the canvas' size
+  // and move it to the center of the canvas.
+  planet.projection
+    .scale(canvas.width / 2)
+    .translate([canvas.width / 2, canvas.height / 2]);
+  planet.draw(canvas);
