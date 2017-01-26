01c9dfbc3d2aa3fb959459cb1b576a7c70f75bd8
Petr Ševčík <sevcik.cartography@gmail.com>
2017-01-26 19:29:53 +0100

    a

--- planet_one.js
+++ planet_one.js
@@ -37,32 +37,3 @@ var canvas = document.getElementById('rotatingGlobe');
   // Draw that globe!
 globe.draw(canvas);
 
-function autorotate(degPerSec) {
-    // Planetary.js plugins are functions that take a `planet` instance
-    // as an argument...
-    return function(planet) {
-      var lastTick = null;
-      var paused = false;
-      planet.plugins.autorotate = {
-        pause:  function() { paused = true;  },
-        resume: function() { paused = false; }
-      };
-      // ...and configure hooks into certain pieces of its lifecycle.
-      planet.onDraw(function() {
-        if (paused || !lastTick) {
-          lastTick = new Date();
-        } else {
-          var now = new Date();
-          var delta = now - lastTick;
-          // This plugin uses the built-in projection (provided by D3)
-          // to rotate the globe each time we draw it.
-          var rotation = planet.projection.rotate();
-          rotation[0] += degPerSec * delta / 1000;
-          if (rotation[0] >= 180) rotation[0] -= 360;
-          planet.projection.rotate(rotation);
-          lastTick = now;
-        }
-      });
-    };
-  };
-})();
