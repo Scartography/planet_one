48845b3660da5f2bb6ee33eee7db315034d65fe0
Petr Ševčík <sevcik.cartography@gmail.com>
2017-01-26 19:27:58 +0100

    s

--- planet_one.js
+++ planet_one.js
@@ -4,13 +4,27 @@
   globe.loadPlugin(autorotate(10));
 // You can remove this statement if `world-110m.json`
 // is in the same path as the HTML page:
-planet.loadPlugin(planetaryjs.plugins.earth({
+globe.loadPlugin(planetaryjs.plugins.earth({
 	topojson: { file: 'world-110m.json' }
 	}));
 // Make the planet fit well in its canvas
-planet.loadPlugin(planetaryjs.plugins.drag());
+globe.loadPlugin(planetaryjs.plugins.drag());
 
-planet.projection.scale(250).translate([250, 250]);
+globe.loadPlugin(planetaryjs.plugins.zoom({
+    scaleExtent: [100, 300]
+  }));
+globe.loadPlugin(planetaryjs.plugins.drag({
+    // Dragging the globe should pause the
+    // automatic rotation until we release the mouse.
+    onDragStart: function() {
+      this.plugins.autorotate.pause();
+    },
+    onDragEnd: function() {
+      this.plugins.autorotate.resume();
+    }
+  }));
+
+globe.projection.scale(250).translate([250, 250]);
 var canvas = document.getElementById('rotatingGlobe');
   // Special code to handle high-density displays (e.g. retina, some phones)
   // In the future, Planetary.js will handle this by itself (or via a plugin).
